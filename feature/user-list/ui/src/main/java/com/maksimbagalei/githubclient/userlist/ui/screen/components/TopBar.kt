3e311c8b95b0c2a0226ebf741a6fcae8c7c9d575
maksim_bagalei <poeismylife@gmail.com>
2024-04-21 20:43:52 +0900

    Replace remember with rememberSaveable.

--- feature/user-list/ui/src/main/java/com/maksimbagalei/githubclient/userlist/ui/screen/components/TopBar.kt
+++ feature/user-list/ui/src/main/java/com/maksimbagalei/githubclient/userlist/ui/screen/components/TopBar.kt
@@ -14,7 +14,7 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -41,7 +41,7 @@ fun TopBar(
 
 @Composable
 fun TopBarTitle(onUserSearch: (String) -> Unit) {
-    var search by remember { mutableStateOf("") }
+    var search by rememberSaveable { mutableStateOf("") }
     TextField(
         modifier = Modifier
             .fillMaxWidth(),
