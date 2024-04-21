6d59d07a8101179a320f6abe18ab7b9b45534d3d
maksim_bagalei <poeismylife@gmail.com>
2024-04-22 01:59:02 +0900

    Fix RepositoryToRepositoryModelMapper di.

--- feature/user-details/ui/src/main/java/com/maksimbagalei/githubclient/userdetails/ui/mapper/RepositoryToRepositoryModelMapper.kt
+++ feature/user-details/ui/src/main/java/com/maksimbagalei/githubclient/userdetails/ui/mapper/RepositoryToRepositoryModelMapper.kt
@@ -3,8 +3,9 @@ package com.maksimbagalei.githubclient.userdetails.ui.mapper
 import com.maksimbagalei.githubclient.common.ui.mapper.Mapper
 import com.maksimbagalei.githubclient.userdetails.data.dto.Repository
 import com.maksimbagalei.githubclient.userdetails.ui.model.RepositoryModel
+import javax.inject.Inject
 
-internal class RepositoryToRepositoryModelMapper : Mapper<Repository, RepositoryModel> {
+internal class RepositoryToRepositoryModelMapper @Inject constructor() : Mapper<Repository, RepositoryModel> {
     override fun map(from: Repository): RepositoryModel = RepositoryModel(
         name = from.name,
         language = from.language,
