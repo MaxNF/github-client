4a2deed799f7d62d6b5b9f9baf0fea4b36b2b657
maksim_bagalei <poeismylife@gmail.com>
2024-04-22 00:37:20 +0900

    Add additional dependencies to user-detail module.

--- feature/user-details/ui/build.gradle.kts
+++ feature/user-details/ui/build.gradle.kts
@@ -8,5 +8,13 @@ dependencies {
     implementation(project(":feature:user-details:data"))
     implementation(project(":common:ui"))
 
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.paging.compose)
+    implementation(libs.androidx.compose.tooling.preview)
+    implementation(libs.shimmer.compose)
+    implementation(libs.coil)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.hilt.navigation.compose)
     testImplementation(project(":common:test"))
+    debugImplementation(libs.androidx.ui.tooling)
 }
